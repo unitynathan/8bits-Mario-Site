f902d16137c97cb32203687cb42caf643f408d5a
Unitynathan - Nathan Moraes <dmoraesabreu_nathan404@outlook.com>
2023-07-14 15:12:58 -0300

    Barra de navegação e inicio do darkmode

--- Scripts/8bits-mario-site.js
+++ Scripts/8bits-mario-site.js
@@ -1,3 +1,6 @@
+const chk = document.getElementById('chk')
 
+//Muda a cor do site quando o botão "darkmode" é pressionado
+chk.addEventListener('change', () => {document.body.classList.toggle('dark')})
 
 
